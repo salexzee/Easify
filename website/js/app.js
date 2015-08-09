bdda5a4c9e8e4f27b2f9332920b1b4428c012198
salexzee <samwebb@codingtestsite.com>
2015-08-09 20:11:25 +0930

    added the start for some other pages

--- website/js/app.js
+++ website/js/app.js
@@ -1,16 +1,34 @@
 var easifyApp = angular.module('easifyApp', ['ngRoute']);
 
 easifyApp.config(function($routeProvider) {
-    
+
  $routeProvider
  
  .when('/', {
-     templateUrl: 'pages/main.html',
-     controller: 'mainController'
+   templateUrl: 'pages/main.html',
+   controller: 'mainController'
+ })
+ 
+ .when('/docs', {
+   templateUrl: 'pages/docs.html',
+   controller: 'docsController'
+ })
+ 
+ .when('/contribute', {
+   templateUrl: 'pages/contribute.html',
+   controller: 'contributeController'
  });
 
 });
 
 easifyApp.controller('mainController' ,['$scope', function ($scope) {
+
+}]);
+
+easifyApp.controller('docsController' ,['$scope', function ($scope) {
+
+}]);
+
+easifyApp.controller('contributeController' ,['$scope', function ($scope) {
 
 }]);
