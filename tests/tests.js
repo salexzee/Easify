52ec4120b24cc6feeca03a3485d417c0eba871bf
salexzee <samwebb@codingtestsite.com>
2015-08-15 19:14:10 +0930

    added more tests

--- tests/tests.js
+++ tests/tests.js
@@ -30,7 +30,11 @@
   // * set new text variable for every test
   // * if for passing test, else for failing test
 
-  // Test for capitalize method
+  // This section is purposefully repetitive to make sure all
+  // tests can be easily read and created. Stick to the provided
+  // format.
+
+  // Checks if capitalize() returns a string with the first letter capitalized
   tests.push(
     function() {
       var text = "capitalize returns a new string with the first letter capitalized"
@@ -42,7 +46,7 @@
     }
   );
 
-  // Test to check isString returns true
+  // Checks if isString() returns true
   tests.push(
     function() {
       var text = "isString returns true if input value is of type 'string'";
@@ -54,12 +58,47 @@
     }
   );
 
-  // Test to check isString returns false
+  // Checks if isString() returns false
   tests.push(
     function() {
       var text = "isString returns false if input value is not of type 'string'";
       if (e.isString(testNumber) === false) {
         testOutput(text, 'passed');
+      } else {
+        testOutput(text, 'failed');
+      }
+    }
+  );
+
+  // Checks if last() returns the last letter of a string
+  tests.push(
+    function() {
+      var text = "last returns the last letter in a string";
+      if (e.last(testWord) === 'g') {
+        testOutput(text, 'passed');
+      } else {
+        testOutput(text, 'failed');
+      }
+    }
+  );
+
+  // Checks if remove takes out the provided amount of letters
+  tests.push(
+    function() {
+      var text = 'remove returns a string with letters removed';
+      if (e.remove(testWord, testNumber).length === testWord.length - testNumber ) {
+        testOutput(text, 'passed');
+      } else {
+        testOutput(text, 'failed');
+      }
+    }
+  );
+
+  tests.push(
+    function() {
+      var text = 'removeAll returns a string with all of a specified letter removed';
+      if (e.removeAll(testWord, 't') === 'esing') {
+        testOutput(text, 'passed'); 
       } else {
         testOutput(text, 'failed');
       }
