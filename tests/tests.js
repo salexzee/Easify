bb3af275b802522f43c38b82a504cfcdfddcca90
salexzee <samwebb@codingtestsite.com>
2015-08-17 21:15:02 +0930

    more tests

--- tests/tests.js
+++ tests/tests.js
@@ -43,7 +43,7 @@
   // Checks if capitalize() returns a string with the first letter capitalized
   tests.push(
     function() {
-      var text = "capitalize returns a new string with the first letter capitalized"
+      var text = "capitalize() returns a new string with the first letter capitalized"
       if (e.capitalize(testWord) === 'Testing') {
         testOutput(text, 'passed');
       } else {
@@ -55,7 +55,7 @@
   // Checks if isString() returns true
   tests.push(
     function() {
-      var text = "isString returns true if input value is of type 'string'";
+      var text = "isString() returns true if input value is of type 'string'";
       if (e.isString(testWord) === true) {
         testOutput(text, 'passed');
       } else {
@@ -67,7 +67,7 @@
   // Checks if isString() returns false
   tests.push(
     function() {
-      var text = "isString returns false if input value is not of type 'string'";
+      var text = "isString() returns false if input value is not of type 'string'";
       if (e.isString(testNumber) === false) {
         testOutput(text, 'passed');
       } else {
@@ -79,7 +79,7 @@
   // Checks if last() returns the last letter of a string
   tests.push(
     function() {
-      var text = "last returns the last letter in a string";
+      var text = "last() returns the last letter in a string";
       if (e.last(testWord) === 'g') {
         testOutput(text, 'passed');
       } else {
@@ -91,7 +91,7 @@
   // Checks if remove() takes out the provided amount of letters
   tests.push(
     function() {
-      var text = 'remove returns a string with letters removed';
+      var text = 'remove() returns a string with letters removed';
       if (e.remove(testWord, testNumber).length === testWord.length - testNumber ) {
         testOutput(text, 'passed');
       } else {
@@ -103,7 +103,7 @@
   // Checks if removeAll() takes out all occurances of the provided letter
   tests.push(
     function() {
-      var text = 'removeAll returns a string with all of a specified letter removed';
+      var text = 'removeAll() returns a string with all of a specified letter removed';
       if (e.removeAll(testWord, 't') === 'esing') {
         testOutput(text, 'passed'); 
       } else {
@@ -115,7 +115,7 @@
   // Checks if randomize() returns a string of the same length as the provided string
   tests.push(
     function() {
-      var text = 'randomize returns a string of the same length of the input string';
+      var text = 'randomize() returns a string of the same length of the input string';
       if (e.randomize(testWord).length === testWord.length) {
         testOutput(text, 'passed');
       } else {
@@ -132,8 +132,44 @@
   // the same string.
   tests.push(
     function() {
-      var text = 'randomize returns a string that is not the same as the input word';
+      var text = 'randomize() returns a string that is not the same as the input word';
       if(e.randomize(testSentence) !== testSentence) {
+        testOutput(text, 'passed');
+      } else {
+        testOutput(text, 'failed');
+      }
+    }
+  );
+
+  // Checks if repeat() returns the provided string repeated the specified amount of times
+  tests.push(
+    function() {
+      var text = 'repeat() returns the provided string repeated the specified amount of times';
+      if (e.repeat(testWord, 3) === 'testingtestingtesting') {
+        testOutput(text, 'passed');
+      } else {
+        testOutput(text, 'failed');
+      }
+    }
+  );
+
+  // Checks if reverse() takes the provided string and returns a backwards copy
+  tests.push(
+    function() {
+      var text = 'reverse() returns the provided string backwawrds';
+      if (e.reverse(testWord) === 'gnitset') {
+        testOutput(text, 'passed');
+      } else {
+        testOutput(text, 'failed');
+      }
+    }
+  );
+
+  // Checks if wrap() encapsulates the provided string in a provided HTML tag
+  tests.push(
+    function() {
+      var text = 'wrap() encapsulates the provided string in a provided HTML tag';
+      if (e.wrap(testWord, 'h1') === '<h1>testing</h1>') {
         testOutput(text, 'passed');
       } else {
         testOutput(text, 'failed');
