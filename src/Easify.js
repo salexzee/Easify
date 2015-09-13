011597c61ce4bc571f5f792dbf02fbd8fca866c3
Luke Shimkus <super99leo@gmail.com>
2015-09-12 20:34:57 -0400

    Fixed if statement

--- src/Easify.js
+++ src/Easify.js
@@ -622,16 +622,17 @@
     
     // Clones an object
     clone: function(obj){
-      if (null == obj || "object" != typeof obj) { 
-        return obj;
-      }
-      var copy = obj.constructor();
-        for (var attr in obj) {
-          if (obj.hasOwnProperty(attr)){ 
-            copy[attr] = obj[attr];
+      if(this.isObject(obj)){
+        var copy = obj.constructor();
+          for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)){ 
+              copy[attr] = obj[attr];
+            }
           }
-        }
-      return copy;
+        return copy;
+      } else {
+        return false; 
+      }
     },
 
     // Converts an object into an array of key/value arrays
