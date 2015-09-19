33ddf8bc18b0bff26ee2c3d9d13ddf49ab4caf3a
salexzee <samwebb@codingtestsite.com>
2015-09-19 21:51:50 +0930

    changed functionality of drop()

--- src/Easify.js
+++ src/Easify.js
@@ -547,17 +547,20 @@
       return;
     },
 
-    // Returns an object with all but the specified keys
+    // Removes the specified methods/properties from the input object 
     drop: function(obj, dropKeys) {
+      // Checks
       if (this.isObject(obj) && validateArray(dropKeys)) {
+        // Grabs the keys from the input object
         var keys = Object.keys(obj);
-        var newObj = {};
+        // Loops over those keys
         for (var i = 0; i < keys.length; i++) {
-          if (!this.has(dropKeys, keys[i])) {
-            newObj[keys[i]] = obj[keys[i]];
+          // Checks if any of the object keys are in the passed in array
+          if (this.has(dropKeys, keys[i])) {
+            // Removes specified keys from the object
+            delete obj[keys[i]];
           }
         }
-        return newObj;
       }
     },
 
