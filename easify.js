1ca5ec84c18b4160f04dede87825bdc205677b53
salexzee <samwebb@codingtestsite.com>
2015-09-23 10:17:51 +0930

    changed some DOM method names to make them easier to remember

--- easify.js
+++ easify.js
@@ -773,21 +773,22 @@
     },
 
     // Returns the DOM element with the passed in ID
-    elementFromId: function(id) {
+    id: function(id) {
       return document.getElementById(id);
     },
 
     // Returns a list of DOM elements with the passed in class
-    elementsFromClass: function(classname) {
+    className: function(classname) {
       return document.getElementsByClassName(classname);
     },
 
     // Returns a list of DOM elements with the passed in tag
-    elementsFromTag: function(tag) {
+    tagName: function(tag) {
       return document.getElementsByTagName(tag);
     },
 
-    elementsFromName: function(name) {
+    // Returns a list of DOM elements with the passed in name
+    name: function(name) {
       return document.getElementsByName(name);
     },
 
