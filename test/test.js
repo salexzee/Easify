21e58c4edd8e1ca52900e13754cc7302e49d0170
salexzee <samwebb@codingtestsite.com>
2015-10-28 18:27:31 +0930

    adds more string tests

--- test/test.js
+++ test/test.js
@@ -11,11 +11,27 @@ describe('Easify', function() {
 
   describe('String Methods', function() {
     it('should capitalize the first letter of the input string', function() {
-      assert.equal(e.cap('hello'), 'Hello');
+      assert.strictEqual(e.cap('hello'), 'Hello');
     });
 
     it('should lower case all letters in the input string', function() {
-      assert.equal(e.downcase('HELLO'), 'hello');
+      assert.strictEqual(e.downcase('HELLO'), 'hello');
+    });
+
+    it('should check if the argument is of type string', function() {
+      assert.isTrue(e.string('Hello'));
+    });
+
+    it('should return the last letter of the string argument', function() {
+      assert.strictEqual(e.last('hello'), 'o');
+    });
+
+    it('should return a string of characters as long as the specified argument', function() {
+      assert.lengthOf(e.password(12), 12);
+    });
+
+    it('should remove the specified amount of characters from the provided string', function() {
+      assert.lengthOf(e.remove('hello', 2), 3);
     });
   });
 });
