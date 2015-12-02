4ffbb243178cb0a6392e3213c7ded2119707b424
salexzee <samwebb@codingtestsite.com>
2015-12-02 10:29:39 +0930

    update to 0.8.5

--- easify.js
+++ easify.js
@@ -120,7 +120,7 @@ if(typeof(window) === 'undefined') {
     return new Easify.init();
   }
 
-  Easify.VERSION = '0.8.4';
+  Easify.VERSION = '0.8.5';
 
   // Function that actually creates object
   // to remove 'new' keyword for users
